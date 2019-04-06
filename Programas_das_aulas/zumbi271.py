ccd01e2609aa6045ff288b4d213cdbff4707532a
Aline Gallo De Mitri <alinegmitri@gmail.com>
2019-04-05 22:12:46 -0300

    Correções no exercício da aula 271

--- Programas_das_aulas/zumbi271.py
+++ Programas_das_aulas/zumbi271.py
@@ -1,7 +1,8 @@
 notas = [8.5, 9.0, 6.5, 7.0, 10.0]
 n = 0
+soma = 0
 while n < 5 :
-    soma += nota [n]
+    soma += notas [n]
     n = n + 1
 média = soma / 5
-print ('Média = ' média)
+print ('Média = ', média)
