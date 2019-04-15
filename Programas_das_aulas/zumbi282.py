a4085c459b1e6c63dc847149c00d62e1568ae510
Aline Gallo De Mitri <alinegmitri@gmail.com>
2019-04-15 20:44:06 -0300

    Inclusão de novos programas das aulas TWP282 e TWP285

---
+++ Programas_das_aulas/zumbi282.py
@@ -0,0 +1,18 @@
+Python 3.7.2 (tags/v3.7.2:9a3ffc0492, Dec 23 2018, 22:20:52) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> x = 'abacate'
+>>> y = "McDonald's"
+>>> y
+"McDonald's"
+>>> x = "0123456789"
+>>> print (x[0:2])
+01
+>>> print (x [1 : 4])
+123
+>>> print (x [ : 3])
+012
+>>> print (x [6 : ])
+6789
+>>> print (x [-4 : -1])
+678
+>>> 
